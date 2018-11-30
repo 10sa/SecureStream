3fcdca17819dd3bf0a1f3b5929fdd5df39c3d528
10sa <zenkins26@gmail.com>
2018-11-30 10:04:51 +0900

    Initialize UnitTest project.

---
+++ CryptoMemoryStreamTests/IO/CryptoMemoryStreamTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoMemoryStream.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoMemoryStream.IO.Tests
+{
+	[TestClass()]
+	public class CryptoMemoryStreamTests
+	{
+		[TestMethod()]
+		public void WriteTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
