35035c303ced1bb1f6442a09721fb46d6f3edacf
10sa <zenkins26@gmail.com>
2018-12-04 15:36:34 +0900

    Add constructors, Override Close() method.

--- CryptoMemoryStream/IO/CryptoMemoryStream.cs
+++ CryptoMemoryStream/IO/CryptoMemoryStream.cs
@@ -13,7 +13,7 @@ namespace CryptoMemoryStream.IO
 	/// </summary>
 	public class CryptoMemoryStream : MemoryStream
 	{
-		private ICryptoTransform crypter;
+		private ICryptoTransform cryptor;
 
 		private AesCryptoServiceProvider aesManaged = new AesCryptoServiceProvider
 		{
@@ -22,17 +22,51 @@ namespace CryptoMemoryStream.IO
 		};
 
 		/// <summary>
+		/// 지정된 바이트 배열을 기반으로 하는 CryptoMemoryStream 클래스의 크기 조정이 불가능한 새 인스턴스를 초기화합니다.
+		/// </summary>
+		/// <param name="buffer">현재 스트림을 만들 부호 없는 바이트의 배열입니다.</param>
+		/// /// <param name="key">암호화 및 복호화에 사용되는 키 값입니다.</param>
+		public CryptoMemoryStream(byte[] buffer, byte[] key) : base(buffer)
+		{
+			InitializeCryptor(key);
+		}
+
+		/// <summary>
 		/// 지정된 대로 초기화된 확장명 가능한 용량을 사용하고 지정된 키 값으로 암호화 되는 CryptoMemoryStream 클래스의 새 인스턴스를 초기화합니다.
 		/// </summary>
 		/// <param name="size">내부 배열의 초기 크기(바이트)입니다.</param>
 		/// <param name="key">암호화 및 복호화에 사용되는 키 값입니다.</param>
-		public CryptoMemoryStream(int size, byte[] key)  : base(size)
+		public CryptoMemoryStream(int size, byte[] key) : base(size)
 		{
+			InitializeCryptor(key);
+		}
+
+		/// <summary>
+		/// 0으로 초기화된 확장명 가능한 용량을 사용하여 CryptoMemoryStream 클래스의 새 인스턴스를 초기화합니다.
+		/// </summary>
+		/// <param name="key">암호화 및 복호화에 사용되는 키 값입니다.</param>
+		public CryptoMemoryStream(byte[] key) : base()
+		{
+			InitializeCryptor(key);
+		}
+
+		/// <summary>
+		/// 초기화된 MemoryStream 클래스의 인스턴스를 이용하여 CryptoMemoryStream을 생성합니다.
+		/// </summary>
+		/// <param name="baseStream">GetBuffer() 메소드를 호출하여 버퍼를 획득할 MemoryStream 클래스의 인스턴스입니다.</param>
+		/// <param name="key">암호화 및 복호화에 사용되는 키 값입니다.</param>
+		/// <returns>전달된 MemoryStream 인스턴스의 GetBuffer() 메소드를 호출하여 얻은 버퍼와 키 값으로 초기화된 CryptoMemoryStream 클래스의 인스턴스입니다.</returns>
+		public static CryptoMemoryStream Create(MemoryStream baseStream, byte[] key)
+		{
+			return new CryptoMemoryStream(baseStream.GetBuffer(), key);
+		}
+
+		private void InitializeCryptor(byte[] key)
+		{
+			cryptor = aesManaged.CreateEncryptor();
 			aesManaged.KeySize = key.Length * 8;
 			aesManaged.Key = key;
 			aesManaged.IV = key;
-
-			crypter = aesManaged.CreateEncryptor();
 		}
 
 		/// <summary>
@@ -45,7 +79,7 @@ namespace CryptoMemoryStream.IO
 		{
 			byte[] encryptBytes = (byte[])buffer.Clone();
 
-			CTRAlgorithm(encryptBytes, offset, count);
+			CTRCryptor(encryptBytes, offset, count);
 			base.Write(encryptBytes, offset, count);
 		}
 
@@ -86,12 +120,23 @@ namespace CryptoMemoryStream.IO
 		public int Decrypt(byte[] buffer, int offset, int count)
 		{
 			int readedSize = Read(buffer, offset, count);
-			CTRAlgorithm(buffer, offset, readedSize);
+			CTRCryptor(buffer, offset, readedSize);
 
 			return readedSize;
 		}
 
-		private void CTRAlgorithm(byte[] buffer, int offset, int count)
+		/// <summary>
+		/// 현재 스트림을 닫고 현재 스트림과 관련된 소켓과 파일 핸들 등의 리소스를 모두 해제합니다. 이 메서드를 호출하는 대신 스트림이 올바르게 삭제되었는지 확인합니다.
+		/// </summary>
+		public override void Close()
+		{
+			cryptor.Dispose();
+			aesManaged.Dispose();
+
+			base.Close();
+		}
+
+		private void CTRCryptor(byte[] buffer, int offset, int count)
 		{
 			Queue<byte> xorMask = new Queue<byte>();
 			int blockSize = aesManaged.BlockSize / 8;
@@ -102,7 +147,7 @@ namespace CryptoMemoryStream.IO
 				{
 					var counterModeBlock = new byte[blockSize];
 
-					crypter.TransformBlock(counter, 0, counter.Length, counterModeBlock, 0);
+					cryptor.TransformBlock(counter, 0, counter.Length, counterModeBlock, 0);
 
 					for (var _i = counter.Length - 1; _i >= 0; _i--)
 					{
